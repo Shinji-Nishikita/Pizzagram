e97a722e2f1fbd078431a938657252a64bbf871f
Shinji-Nishikita <pizattu@gmail.com>
2021-12-03 23:56:10 +0900

    added proxy in package.json/imported knexfile for config/get the data from posts table

--- server/server.js
+++ server/server.js
@@ -2,18 +2,23 @@
 require("dotenv").config();
 //expressを読み込む
 const express = require("express");
-const port = process.env.PORT || 3000;
-
 //expressサーバーを作成する
 const app = express();
+const port = process.env.PORT || 3000;
+//knexfileを読み込む
+const config = require("../knexfile");
+const knex = require("knex")(config[process.env.DB_ENV]);
 
 //クライアント側から送信されたリクエストをreq.bodyで取得するためにJSONをパースするミドルウェアを使用する。
 app.use(express.json());
 
-//リクエストを受信できるかテスト
-app.get("/test", (req, res) => {
-    res.send("getできてますね！");
-})
+//データベースの"posts"テーブルから情報を取得する
+//非同期通信(async-await)
+app.get("/test", async (req, res) => {
+    const data = await knex.select().from("posts");
+    console.log("data is", data);
+    res.send(data)
+});
 
 //listenメソッドを実行して指定したポート番号でリクエストを待ち受ける
 app.listen(port, () => {
