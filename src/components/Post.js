645e765a76857ea85c9db55ad794fb0bce8f3fe3
Shinji-Nishikita <pizattu@gmail.com>
2021-12-07 21:17:54 +0900

    added input form and button for comment/fixed css

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import "./post.css";
 import avatar from "./avatar.png";
+import { useState, useEffect } from "react";
 
 export default function Post({ photo }) {
+  const [comments, setComments] = useState([]);
+  // console.log("photoデータは:", photo)
+  useEffect(() => {
+    async function getComment() {
+      const data = await fetch(`/comments/${photo.id}`)
+      const parse = await data.json();
+      console.log("parseは:", parse)
+      setComments(parse)
+    }
+    getComment()
+  }, [photo.id])
+
   return (
     <div className="container">
       <div className="postHead">
@@ -12,7 +25,26 @@ export default function Post({ photo }) {
       <div>
         <img className="photo" src={photo.photo_url} alt={photo.id}/>
       </div>
-      <div className="comments">comments</div>
+      {comments.map((item) => {
+        return (
+          <div className="comments" key={item.id}>
+            <div className="commentUser">{item.user}</div>
+            <div className="commentText">{item.text}</div>
+          </div>
+        )
+      })}
+      {comments.map((item) => {
+        return (
+          <div className="addComment">
+            <input
+              className="inputText"
+              type="text"
+              placeholder={`${item.user}としてコメントを追加する`}
+            />
+            <button className="button">send</button>
+          </div>
+        )
+      })}
     </div>
   );
 };
