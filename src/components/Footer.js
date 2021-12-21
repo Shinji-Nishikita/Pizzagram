87fb83bc867c4afb55410ca8ec4b967c761a3be0
Shinji-Nishikita <pizattu@gmail.com>
2021-12-22 00:21:18 +0900

    installed firebase

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,29 @@ import camera from "./camera.png";
 export default function Footer() {
 
   // upload関数
-  async function upload() {
+  async function upload(e) {
+    console.log("eは:", e)
+    if (!e.target.files.length) return;
+    const fileOfPics = e.target.files[0]
+    console.log("fileOfPicsは:", fileOfPics)
 
+    await fetch(
+      process.env.IMGBB_URL,
+      {
+      method: "POST",
+      headers: {
+      "Content-Type": "Access-Control-Allow-Headers"
+      },
+      body: fileOfPics,
+      }
+    )
   }
   return (
     <div className="footer">
       <div className="footerBack"></div>
       <div className="cameraIcon">
         <img className="cameraImg" src={camera} alt="cam" />
-        <input type="file" onChange={upload}></input>
+        <input id="input_img" type="file" accept="image/*" onChange={upload}></input>
       </div>
     </div>
   )
