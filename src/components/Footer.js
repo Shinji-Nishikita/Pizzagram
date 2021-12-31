8afe91bc9a4fad86cdbbb368122e8d344fdfba06
Shinji-Nishikita <pizattu@gmail.com>
2021-12-31 23:58:12 +0900

    fixed s3-config

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import S3 from 'react-aws-s3';
 import "./footer.css";
 import camera from "./camera.png";
+require("dotenv").config();
 
 export default function Footer() {
 
+  const postButton = useRef();
   const config = {
-    bucketName: 'test',
-    dirName: 'test', /* optional */
-    region: 'test',
-    accessKeyId: 'test',
-    secretAccessKey: 'test',
-    s3Url: 'test', /* optional */
-  }
-
+    bucketName: "pizzagram-s3",
+    region: "ap-northeast-1",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
   const ReactS3Client = new S3(config);
 
   // upload関数
   async function upload(e) {
     // console.log("eは:", e)
+    // console.log("postButton is", postButton)
     if (!e.target.files.length) return;
     const fileOfPics = e.target.files[0]
     // console.log("fileOfPicsは:", fileOfPics)
@@ -33,8 +33,10 @@ export default function Footer() {
     <div className="footer">
       <div className="footerBack"></div>
       <div className="cameraIcon">
-        <img className="cameraImg" src={camera} alt="cam" />
-        <input id="input_img" type="file" accept="image/*" onChange={upload}></input>
+        <img className="cameraImg" src={camera} alt="cam" onClick={() => {
+          postButton.current.click();
+        }}/>
+        <input id="input_img" type="file" ref={postButton} onChange={upload}></input>
       </div>
     </div>
   )
